e9f2e52c2166afc5ad86a581b5ef5db59d54ef48
aadii104 <sharmaadi104@gmail.com>
2018-11-03 15:34:53 +0530

    index file edited.

--- src/main.js
+++ src/main.js
@@ -1,44 +1,15 @@
-require('../node_modules/vuetify/src/stylus/app.styl')
-
-import Vue from 'vue'
-import {
-  Vuetify,
-  VApp,
-  VNavigationDrawer,
-  VFooter,
-  VList,
-  VBtn,
-  VIcon,
-  VGrid,
-  VToolbar,
-  transitions
-} from 'vuetify'
-import App from './App.vue'
-
-Vue.use(Vuetify, {
-  components: {
-    VApp,
-    VNavigationDrawer,
-    VFooter,
-    VList,
-    VBtn,
-    VIcon,
-    VGrid,
-    VToolbar,
-    transitions
-  },
-  theme: {
-    primary: '#ee44aa',
-    secondary: '#424242',
-    accent: '#82B1FF',
-    error: '#FF5252',
-    info: '#2196F3',
-    success: '#4CAF50',
-    warning: '#FFC107'
-  }
-})
-
+require("../node_modules/vuetify/src/stylus/app.styl");
+import ve from "./vuetify.theme.js";
+import Vue from "vue";
+import App from "./App.vue";
+// import router from "./router/index";
+// import store from "./store/store";
 new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+  el: "#app",
+  // router,
+  // store,
+  components: {
+    App
+  },
+  template: "<App/>"
+});
---
+++ src/vuetify.theme.js
@@ -0,0 +1,68 @@
+import Vue from "vue";
+import {
+  Vuetify,
+  VApp,
+  VDataTable,
+  VFooter,
+  VList,
+  VNavigationDrawer,
+  VCard,
+  VSnackbar,
+  VTabs,
+  VCarousel,
+  VParallax,
+  VAvatar,
+  VExpansionPanel,
+  VJumbotron,
+  VForm,
+  VBtn,
+  VIcon,
+  VGrid,
+  VSelect,
+  VRadioGroup,
+  VToolbar,
+  transitions,
+  VSubheader,
+  VTextField,
+  VStepper,
+  VDivider,
+  VMenu,
+  VCheckbox,
+  VAutocomplete
+} from "vuetify";
+
+Vue.use(Vuetify, {
+  theme: {
+    sk_white: "#fff"
+  },
+  components: {
+    VApp,
+    VRadioGroup,
+    VDataTable,
+    VAvatar,
+    VSelect,
+    VCarousel,
+    VSnackbar,
+    VExpansionPanel,
+    VParallax,
+    VJumbotron,
+    VNavigationDrawer,
+    VFooter,
+    VList,
+    VTabs,
+    VBtn,
+    VCard,
+    VForm,
+    VIcon,
+    VGrid,
+    VToolbar,
+    transitions,
+    VSubheader,
+    VTextField,
+    VStepper,
+    VDivider,
+    VMenu,
+    VCheckbox,
+    VAutocomplete
+  }
+});
